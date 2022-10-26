83c2fd624f53cf6595380f4e45d5c922bab96c79
EmilioRdz19 <elmillo2010@live.com.mx>
2022-10-26 18:18:07 -0500

    Modificaciones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain="dev-b7zl4zvnj2z0tf26.us.auth0.com" 
+    clientId="c4wlhowROGMzZP7HeOEap9AYFs7JOqLq" 
+    redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
